c5ffa06c8d1de178e7ad579c4995e5616b0e991e
Sam Adams <sbadams@gmail.com>
2017-04-02 22:00:49 +0100

    Initial commit

---
+++ index.ios.js
@@ -0,0 +1,4 @@
+'use strict';
+import { AppRegistry } from 'react-native';
+import RTStarter from './build';
+AppRegistry.registerComponent('reacttypescriptstarter', () => RTStarter);
---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+export default class RTStarter extends Component<any, any> {
+    render() {
+        return (
+            <View>
+                <Text>
+                    A React-Native TypeScript Starter!
+                </Text>
+            </View>
+        );
+    }
+}
