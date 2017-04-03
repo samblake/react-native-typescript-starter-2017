8eea6689250105770e4da09a0f121e8fd0991272
Sam Adams <sbadams@gmail.com>
2017-04-03 22:18:43 +0100

    Updated dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
-import {
-    StyleSheet,
-    Text,
-    View
-} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default class RTStarter extends Component<any, any> {
     render() {
