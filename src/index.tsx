ab6ad0908f127c7c2de9720b4e0ace5d9a3f4c6c
Sam Adams <sbadams@gmail.com>
2017-04-04 20:48:28 +0100

    Add style

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ViewStyle, TextStyle, StyleSheet, Text, View } from 'react-native';
 
 export default class RTStarter extends Component<any, any> {
     render() {
         return (
-            <View>
-                <Text>
+            <View style={styles.container}>
+                <Text style={styles.text}>
                     A React-Native &amp; TypeScript Starter!
                 </Text>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    } as ViewStyle,
+
+    text: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    } as TextStyle,
+});
